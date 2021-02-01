e9de18bf6409297c43d88fd31590fec2f6c25c03
indratdev <indrat.dev@gmail.com>
2021-02-01 19:48:48 +0700

    Initial Commit

---
+++ Mey Food/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mey Food
+//
+//  Created by IndratS on 01/02/21.
+//  Copyright © 2021 IndratSaputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
